0fe3d1d1aa566f8708efbdf003c5eae3bd3b9568
choijake0207 <jake.ch0712@gmail.com>
2024-08-28 16:34:12 -0700

    create landing quote section

---
+++ client/src/Components/LandingComp/Quote.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Quote() {
+  return (
+    <section className="landing-quote">
+        <p className="prim-quote">
+            Whether you're <span className="hl-green">starting something new</span> <br/>
+            or <span className="hl-red">quitting something old</span>, <br/>
+            habits shape who we become...
+        </p>
+    </section>
+  )
+}
