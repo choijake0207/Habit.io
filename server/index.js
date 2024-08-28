d373bd733f13d2dae877269167646b57cf0f9bab
choijake0207 <jake.ch0712@gmail.com>
2024-08-28 16:17:49 -0700

    create server entry point

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const db = require("./models")
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {console.log("Server Listening To Port 3001")})
+})
