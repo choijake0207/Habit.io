42fcebd11b9182c370acd0d90b07b73b014e0a87
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 14:48:14 -0700

    create protected routes based off authstatus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import React, { useContext } from 'react'
+import {createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider} from "react-router-dom"
 import RootLayout from './Layouts/RootLayout'
 import LandingPage from './Pages/LandingPage/LandingPage'
 import LoginPage from './Pages/Login/LoginPage'
 import Register from './Pages/Register/Register'
 import { AuthProvider } from './Context/AuthContext'
+import { AuthContext } from './Context/AuthContext'
 import "./App.css"
+
+const PrivateRoutes = ({element}) => {
+  const {authorizedUser} = useContext(AuthContext)
+  return authorizedUser.status ? element : <Navigate to="/login"/>
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<LandingPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/register" element={<Register/>}/>
-    </Route>
+    <>
+      <Route path="/">
+        <Route index element={<LandingPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Route>
+
+      <Route element={<PrivateRoutes element={<RootLayout/>}/>}>
+        <Route/>
+      </Route>
+    </>
   )
 )
 
