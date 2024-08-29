26587bab79483b09167b947ef1649836332ba5c3
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 13:53:31 -0700

    add route for authentication check and token validation

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -3,8 +3,17 @@ const router = express.Router()
 const {User} = require("../models")
 const bcrypt = require("bcrypt")
 const {sign} = require("jsonwebtoken")
+const {validateToken} = require("../middleware/AuthMiddleware")
 
-
+// persistent auth check
+router.get("/auth",validateToken, async (req, res) => {
+    try {
+        const user = await User.findOne({where: {id: req.user.id}, attributes: ["email", "id"]})
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({error: "Failed To Authorize User"})
+    }
+} )
 // register 
 router.post("/register", async (req, res) => {
     const {firstName, lastName, email, password} = req.body
@@ -12,7 +21,7 @@ router.post("/register", async (req, res) => {
         // check for existing email
         const existingEmail = await User.findOne({where: {email: email}})
         if (existingEmail) {
-            return res.status(400).json({error: "Username Already Exists"})
+            return res.status(400).json({error: "Email Already Exists"})
         }
         const hash = await bcrypt.hash(password,10)
         const newUser = await User.create({
@@ -34,5 +43,6 @@ router.post("/register", async (req, res) => {
         res.status(500).json({error: "Registration Failed"})
     }
 })
+
 
 module.exports = router
