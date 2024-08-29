4102c2fa94af14d80ecf8c0545cb59b518539094
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 10:42:02 -0700

    mount user router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const db = require("./models")
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+// mount
+const userRouter = require("./routes/UserRouter")
+app.use("/user", userRouter)
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {console.log("Server Listening To Port 3001")})
