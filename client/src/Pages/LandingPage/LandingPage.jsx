9fb6beb886673375184af8524f4d55d98b0cd75b
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 14:59:00 -0700

    add conditional nav link render

--- client/src/Pages/LandingPage/LandingPage.jsx
+++ client/src/Pages/LandingPage/LandingPage.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Contact from '../../Components/LandingComp/Contact'
 import Quote from '../../Components/LandingComp/Quote'
 import Statement from '../../Components/LandingComp/Statement'
 import WalkThrough from '../../Components/LandingComp/WalkThrough'
 import "./Landing.css"
 import {NavLink} from "react-router-dom"
+import { AuthContext } from '../../Context/AuthContext'
 
 export default function LandingPage() {
+  const {authorizedUser} = useContext(AuthContext)
   return (
     <div className="landing-page">
         <nav className="landing-nav">
             <p>Habit.io</p>
-            <div className="landing-nav-a">
+            {authorizedUser.authStatus ? 
+              <div className="landing-nav-a authorized-nav-a">
+                <NavLink>Go To Dashboard</NavLink>
+              </div>
+            :
+              <div className="landing-nav-a">
                 <NavLink to="/register">Create Account</NavLink>
                 <NavLink to="/login">Log In</NavLink>
-            </div>
+              </div>
+            }
         </nav>
         <Quote/>
         <Statement/>
