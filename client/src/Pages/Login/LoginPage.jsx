d33b197508cc50f0b3bc2c2d411140a97a8b7246
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 11:55:30 -0700

    create seperate log/sign pages that render form based off type prop

---
+++ client/src/Pages/Login/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LogReg from '../../Components/Forms/LogRegForm'
+
+export default function LoginPage() {
+  return (
+    <div className="login-page">
+        <LogReg type="login"/>
+    </div>
+  )
+}
---
+++ client/src/Pages/Register/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LogReg from '../../Components/Forms/LogRegForm'
+
+export default function Register() {
+  return (
+    <div className="register-page">
+        <LogReg type="register"/>
+    </div>
+  )
+}
