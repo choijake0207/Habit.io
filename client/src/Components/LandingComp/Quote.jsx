80b0e3198dfbeb5d5e48c4d82a4bbf437ee67170
choijake0207 <jake.ch0712@gmail.com>
2024-08-28 18:11:33 -0700

    add span for typing anim

--- client/src/Components/LandingComp/Quote.jsx
+++ client/src/Components/LandingComp/Quote.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 export default function Quote() {
   return (
     <section className="landing-quote">
-        <p className="prim-quote">
-            Whether you're <span className="hl-green">starting something new</span> <br/>
-            or <span className="hl-red">quitting something old</span>, <br/>
-            habits shape who we become...
-        </p>
+        <div className="prim-wrap">
+            <p className="prim-quote">
+                <span className="line">Whether you're <span className="hl-green">starting something new</span></span><br/>
+                <span className="line">or <span className="hl-red">quitting something old</span>,</span><br/>
+                <span className="line">habits shape who we become...</span>
+            </p>
+        </div>
     </section>
   )
 }
