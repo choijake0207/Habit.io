15770294cf4ac406dbd2ec65a9dbdb3e1d8cfa64
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 11:52:13 -0700

    add name to req body

--- client/src/API/UserAPI.js
+++ client/src/API/UserAPI.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
-export const register = async (email, password) => {
+export const register = async (firstName, lastName, email, password) => {
     const response = await axios.post("http://localhost:3001/user/register", {
+        firstName,
+        lastName,
         email,
         password
     })
