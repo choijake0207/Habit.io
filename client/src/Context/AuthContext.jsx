10e71d1e9dcdda24af57434abb0cb0b9d024301c
choijake0207 <jake.ch0712@gmail.com>
2024-08-29 14:28:30 -0700

    add login context func to call api

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,9 +1,10 @@
-import React, { createContext, useState } from 'react'
-import { register as registerAPI, authorizeUser as authorizeAPI } from '../API/UserAPI'
+import React, { createContext, useState, useEffect } from 'react'
+import { register as registerAPI, authorizeUser as authorizeAPI, login as loginAPI } from '../API/UserAPI'
 
 export const AuthContext = createContext("")
 export const AuthProvider = ({children}) => {
     const [authorizedUser, setAuthorizedUser] = useState({email: "", id: "", authStatus: false})
+    // register
     const register = async (firstName, lastName, email, password) => {
         try {
             const response = await registerAPI(firstName, lastName, email, password)
@@ -14,6 +15,7 @@ export const AuthProvider = ({children}) => {
             throw(error)
         }
     }
+    // auth check
     useEffect(() => {
         const authorizeUser = async () => {
             try {
@@ -26,9 +28,20 @@ export const AuthProvider = ({children}) => {
         }
         authorizeUser()
     }, [])
+    // login 
+    const login = async (email, password) => {
+        try {
+            const response = await loginAPI(email, password)
+            console.log(response.data)
+            localStorage.setItem("accessToken", response.data.accessToken)
+            setAuthorizedUser({email: response.data.username, id: response.data.id, authStatus: true})
+        } catch (error) {
+            throw(error)
+        }
+    }
 
 
-    const values = {register, authorizedUser}
+    const values = {register, authorizedUser, login}
     return (
         <AuthContext.Provider value={values}>
             {children}
