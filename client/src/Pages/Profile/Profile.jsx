21cef32053ed386fda57968d2df85b12e3e6225d
choijake0207 <jake.ch0712@gmail.com>
2024-09-03 16:22:42 -0700

    add useEffect to call profileFEtch on mount

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PrivatePageWrap from '../../Layouts/PrivatePageWrap'
 import "./profile.css"
+import { fetchProfile } from '../../API/UserAPI'
 
 export default function Profile() {
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    const fetchProfileRequest = async () => {
+      try {
+        const response = await fetchProfile()
+        setProfile(response.data)
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProfileRequest()
+  }, [])
+
+
   return (
     <PrivatePageWrap type={"Profile"}>
       <div className="page" id="profile-page">
