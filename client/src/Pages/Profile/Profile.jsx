5fb9c70248c6158470d96c2430b158288929784e
choijake0207 <jake.ch0712@gmail.com>
2024-09-03 15:28:25 -0700

    add components' ui wireframe

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import PrivatePageWrap from '../../Layouts/PrivatePageWrap'
+import "./profile.css"
 
 export default function Profile() {
   return (
     <PrivatePageWrap type={"Profile"}>
-      <div>Profile</div>
+      <div className="page" id="profile-page">
+        <h2>Your Information</h2>
+        <section className="profile-info">
+          Information
+        </section>
+        <h2>Settings</h2>
+        <section className="profile-settings">
+          Settings
+        </section>
+      </div>
     </PrivatePageWrap>
   )
 }
