1c6e4815500dbe74b2ccd15b82cd606b85c8b04f
choijake0207 <jake.ch0712@gmail.com>
2024-09-03 13:32:08 -0700

    add navbar, links and btns

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, NavLink } from 'react-router-dom'
+import {House, ChartBar, User, SignOut} from "phosphor-react"
 import "./layout.css"
 
 export default function RootLayout() {
   return (
     <div className="root-layout">
+        <nav className='user-nav'>
+          <div className="user-nav-a">
+            <NavLink to="/home"><House/></NavLink>
+            <NavLink to="/insights"><ChartBar/></NavLink>
+            <NavLink to="/profile"><User/></NavLink>
+          </div>
+          <button className="sign-out-btn"><SignOut/></button>
+        </nav>
         <Outlet/>
     </div>
   )
