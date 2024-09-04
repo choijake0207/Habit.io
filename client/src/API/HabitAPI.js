412e1e7382902bc4108b804379a7224b999007af
choijake0207 <jake.ch0712@gmail.com>
2024-09-04 11:25:41 -0700

    create post, fetchAll, fetchSingle habit apis

---
+++ client/src/API/HabitAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+export const createHabit = async (name, startDate) => {
+    const response = await axios.post("http://localhost:3001/habit", {
+        name,
+        startDate
+    })
+    return response
+}
+
+export const fetchAllHabits = async () => {
+    const response = await axios.get("http://localhost:3001/habit", {
+        headers: {
+            accessToken: localStorage.getItem("accessToken")
+        }
+    })
+    return response
+}
+
+export const fetchSingleHabit = async (id) => {
+    const response = await axios.get(`http://localhost:3001/habit/${id}`, {
+        headers: {
+            accessToken: localStorage.getItem("accessToken")
+        }
+    })
+    return response
+}
