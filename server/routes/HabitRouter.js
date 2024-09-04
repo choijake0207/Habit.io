08589666392518f21aae7c8c2ca0f57faf3529e5
choijake0207 <jake.ch0712@gmail.com>
2024-09-04 10:27:16 -0700

    create habit post route handler

--- server/routes/HabitRouter.js
+++ server/routes/HabitRouter.js
@@ -4,7 +4,23 @@ const {Habit} = require("../models")
 const {validateToken} = require("../middleware/AuthMiddleware")
 
 
-
+// post habit
+router.post("/", validateToken, async (req, res) => {
+    try {
+        const {name, startDate} = req.body
+        const id = req.user.id
+        const newHabit = await Habit.create({
+            name: name,
+            startDate: startDate,
+            userId: id,
+        })
+        res.json({
+            message: "Habit Created Succesfully"
+        })
+    } catch (error) {
+        res.status(500).json({error: "Failed To Create Habit"})
+    }
+})
 
 
 module.exports = router
