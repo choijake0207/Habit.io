47b8fb5c6d39d03835377ef9c20e87e34b000123
choijake0207 <jake.ch0712@gmail.com>
2024-09-04 15:30:27 -0700

    add optimistic render for new habit / need to fix undefined date prop

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -31,10 +31,21 @@ export default function Home() {
   }, [])
 
   const handleCreateHabit = async (name, startDate) => {
+    const temporaryHabit = {
+      name,
+      startDate,
+      id: "temporary"
+    }
+    setAllHabits(prev => [...prev, temporaryHabit])
     try {
       const response = await createHabit(name, startDate)
+      const createdHabit = response.data
+      setAllHabits(prev => prev.map(habit => 
+        habit.id === temporaryHabit.id ? createdHabit : habit
+      ))
       console.log(response)
     } catch (error) {
+      setAllHabits(prev => prev.filter(habit => habit.id !== temporaryHabit.id))
       console.log(error)
     }
   }
@@ -45,6 +56,7 @@ export default function Home() {
         <section className="daily-quote">
 
         </section>
+        <h2>Your Habits</h2>
         <ul className="habit-container">
           {!loading && allHabits.length > 0 ? (
             allHabits.map(habit => {
