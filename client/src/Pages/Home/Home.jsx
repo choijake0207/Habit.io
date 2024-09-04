b19bdbbf922d5ea3a8711a3ccdc48d76dcb86a20
choijake0207 <jake.ch0712@gmail.com>
2024-09-03 17:14:48 -0700

    add wireframe UI

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import PrivatePageWrap from '../../Layouts/PrivatePageWrap'
-
+import "./Home.css"
 export default function Home() {
   return (
     <PrivatePageWrap type={"Home"}>
-      <div className="home-page"></div>
+      <div className="page" id="home-page">
+        <section className="daily-quote">
+
+        </section>
+        <ul className="habit-container">
+          <li className="habit">Habit 1</li>
+          <li className="habit">Habit 2</li>
+          <li className="habit">Habit 3</li>
+          <li className="habit">Habit 4</li>
+          <li className="habit">Habit 4</li>
+          <li className="habit">Habit 4</li>
+          <li className="habit">Habit 4</li>
+          <li className="habit">Habit 4</li>
+          <li className="habit">Habit 4</li>
+
+        </ul>
+        <section className="home-widgets">
+          <div className="widget">Widget</div>
+        </section>
+      </div>
 
 
     </PrivatePageWrap>
