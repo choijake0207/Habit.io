54b31a539996cd8f2a99476e9aee3beaa55b4131
choijake0207 <jake.ch0712@gmail.com>
2024-09-05 16:04:12 -0700

    add timer toolbar to change format

--- client/src/Components/SingleComponents/HabitSummary.jsx
+++ client/src/Components/SingleComponents/HabitSummary.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Timer from "../Timer/Timer"
 export default function HabitSummary({habit}) {
+  const [time, date] =habit.creationDate.split("-")
+  const [timeSetting, setTimeSetting] = useState("Hours")
+  const timeSettings = ["Hours", "Days", "Weeks", "Months", "Years"]
+  console.log(timeSetting)
   return (
     <section className="habit-summary">
         <h1>{habit.name}</h1>
-        <h2>Current Streak</h2>
-        <div className="summary-timer">
+        <h3>Current Streak</h3>
+        <div className="timer-toolbar">
+          {timeSettings.map(type => {
+            return (
+              <button 
+                onClick={() => setTimeSetting(type)}
+                className={type === timeSetting && `active`}
+              >
+                {type}
+              </button>
+            )
+          })}
+        </div>
         <Timer
             start={habit.startDate}
         />
-        </div>
-        <p>{habit.startDate}</p>
+        <p>Started on {date}</p>
     </section>
   )
 }
