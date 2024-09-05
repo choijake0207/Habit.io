9f873221a484ce1f21cc134091ef729c576a1437
choijake0207 <jake.ch0712@gmail.com>
2024-09-05 10:51:18 -0700

    create conditional header for single page view

--- client/src/Layouts/PrivatePageWrap.jsx
+++ client/src/Layouts/PrivatePageWrap.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { CaretCircleLeft} from "phosphor-react"
+import { useNavigate } from 'react-router-dom'
 
 export default function PrivatePageWrap({children, type}) {
+  const navigate = useNavigate()
   return (
     <div className="private-page-wrap">
-        <header>
-            <h1>{type}</h1>
+        <header className="page-header">
+            {type === "single" ? 
+              <button onClick={() => navigate("/home")}>
+                <CaretCircleLeft className="back-nav-btn"/>
+              </button>
+            : <h1>{type}</h1>
+            }
         </header>
         {children}
     </div>
