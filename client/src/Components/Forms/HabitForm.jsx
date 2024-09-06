72062b37135caf7bcee624341b13e7e935111bc0
choijake0207 <jake.ch0712@gmail.com>
2024-09-06 11:08:00 -0700

    add color piselector

--- client/src/Components/Forms/HabitForm.jsx
+++ client/src/Components/Forms/HabitForm.jsx
@@ -3,12 +3,16 @@ import "./Form.css"
 import { createHabit } from '../../API/HabitAPI'
 
 export default function HabitForm({toggleVisibility, createHabit}) {
+    const colors = ["red", "blue", "green", "orange", "purple"]
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
     const [name, setName] = useState("")
+    const [color, setColor] = useState(randomColor)
+    console.log(color)
     const handleSubmit = async (e) => {
         e.preventDefault()
         const now = new Date()
         const startDate = `${now.toLocaleTimeString()}-${now.toLocaleDateString()}`
-        createHabit(name, startDate)
+        createHabit(name, startDate, color)
         setName("")
         toggleVisibility()
     }
@@ -22,6 +26,20 @@ export default function HabitForm({toggleVisibility, createHabit}) {
                 required
                 placeholder="Habit Name"
             />
+            <label>Pick a color</label>
+            <div className="color-selector">
+                {colors.map(i => {
+                    return (
+                        <button 
+                            type="button"
+                            key={i}
+                            className={`color-selector-btn ${i}`}
+                            id={i === color && "selected"}
+                            onClick={() => setColor(i)} 
+                        ></button>
+                    )   
+                })}
+            </div>
             <button type="submit">Create</button>
             <button type="button" onClick={toggleVisibility}>Cancel</button>
         </form>
