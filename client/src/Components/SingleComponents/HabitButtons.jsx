c0e8a747cd2beb2db0090f7c824ff254a7a26a82
choijake0207 <jake.ch0712@gmail.com>
2024-09-06 12:32:08 -0700

    add click handler for reset button

--- client/src/Components/SingleComponents/HabitButtons.jsx
+++ client/src/Components/SingleComponents/HabitButtons.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Pause, ArrowCounterClockwise, Flag } from 'phosphor-react'
 
-export default function HabitButtons() {
+export default function HabitButtons({onReset, habit}) {
   return (
     <section className="habit-buttons">
         <button className="pause-btn">
             <Pause/>
             Pause
         </button>
-        <button className="reset-btn">
+        <button className="reset-btn" onClick={onReset}>
             <ArrowCounterClockwise/>
             Reset
         </button>
