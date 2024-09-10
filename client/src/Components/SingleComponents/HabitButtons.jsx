85db3ff8e9901c3d6c75bac5dc3c3c33f790dbf2
choijake0207 <jake.ch0712@gmail.com>
2024-09-10 13:37:07 -0700

    add create goal btn

--- client/src/Components/SingleComponents/HabitButtons.jsx
+++ client/src/Components/SingleComponents/HabitButtons.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { Pause, ArrowCounterClockwise, Flag, Play } from 'phosphor-react'
+import { Pause, ArrowCounterClockwise, Target, Play } from 'phosphor-react'
 
 export default function HabitButtons({onReset, habit, onPause}) {
   return (
     <section className="habit-buttons">
-        <button className="pause-btn" onClick={onPause}>
-            {habit.status === "paused" ? <Play/> : <Pause/>}
+        <button className={habit.status === "paused" ? "paused-pause-btn" : "pause-btn"} onClick={onPause}>
+            {habit.status === "paused" ? <Play weight="fill"/> : <Pause weight="fill"/>}
             {habit.status === "paused" ? "Resume" : "Pause"}
         </button>
         <button className="reset-btn" onClick={onReset}>
             <ArrowCounterClockwise/>
             Reset
         </button>
-        <button className="complete-btn">
-            <Flag/>
-            Mark Complete
+        <button className="goal-btn">
+            <Target/>
+            Create Goal
         </button>
     </section>
   )
