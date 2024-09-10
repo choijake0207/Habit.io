7bdf21054a6dc17242113de75a98322057f48a7d
choijake0207 <jake.ch0712@gmail.com>
2024-09-10 12:08:02 -0700

    Create loading component

---
+++ client/src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {dotSpinner} from "ldrs"
+
+export default function Loader() {
+  return (
+    <div>
+      <l-dot-spinner>
+        
+      </l-dot-spinner>
+    </div>
+  )
+}
