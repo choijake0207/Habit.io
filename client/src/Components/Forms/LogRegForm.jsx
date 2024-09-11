271c9db6214fb025628b770f4e77b00983516d4a
choijake0207 <jake.ch0712@gmail.com>
2024-09-10 18:55:29 -0700

    add alerts for login/reg failure cases

--- client/src/Components/Forms/LogRegForm.jsx
+++ client/src/Components/Forms/LogRegForm.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react'
 import { AuthContext } from '../../Context/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import Alert from "../Alerts/Alert"
 import "./Form.css"
 
 export default function LogReg({type}) {
@@ -10,6 +11,11 @@ export default function LogReg({type}) {
   const [lastName, setLastName] = useState("")
   const {register, authorizedUser, login} = useContext(AuthContext)
   const navigate = useNavigate()
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type, action) => {
+    setAlert({message, type, action})
+  }
 
   const handleRegister = async (e) => {
     e.preventDefault()
@@ -21,6 +27,7 @@ export default function LogReg({type}) {
       setPassword("")
       navigate("/home")
     } catch (error) {
+      showAlert(error.response.data.error, "failure", null)
       console.log(error)
     }
   }
@@ -32,64 +39,67 @@ export default function LogReg({type}) {
       setPassword("")
       navigate("/home")
     } catch (error) {
+      showAlert(error.response.data.error, "failure", null)
       console.log(error)
     }
   }
   console.log(authorizedUser)
 
   return (
-    <div className="logreg-wrap">
-      {type === "login" ? 
-        <form className="login-form" onSubmit={handleLogin}>
-          <h1>Welcome Back</h1>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label/>
-          <button type="submit">Log In</button>
-        </form>
-        :
-        <form className="register-form" onSubmit={handleRegister}>
-          <h1>Get Started Today</h1>
-          <label>First Name</label>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <label>Last Name</label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label/>
-          <button type="submit">Join</button>
-        </form>
-      }
-    </div>
-
+    <>
+      {alert && <Alert message={alert.message} type={alert.type} action={alert.action} onClose={()=>setAlert(null)}/>}
+      <div className="logreg-wrap">
+        {type === "login" ? 
+          <form className="login-form" onSubmit={handleLogin}>
+            <h1>Welcome Back</h1>
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label/>
+            <button type="submit">Log In</button>
+          </form>
+          :
+          <form className="register-form" onSubmit={handleRegister}>
+            <h1>Get Started Today</h1>
+            <label>First Name</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label>Last Name</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label/>
+            <button type="submit">Join</button>
+          </form>
+        }
+      </div>
+    </>
         
   )
 }
