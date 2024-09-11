2586ad3ca1c85c2fc46566cf019adce4f5f5c68b
choijake0207 <jake.ch0712@gmail.com>
2024-09-11 10:09:03 -0700

    create and mount goal router

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const userRouter = require("./routes/UserRouter")
 app.use("/user", userRouter)
 const habitRouter = require("./routes/HabitRouter")
 app.use("/habit", habitRouter)
+const goalRouter = require("./routes/HabitRouter")
+app.use("/goal", goalRouter)
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {console.log("Server Listening To Port 3001")})
