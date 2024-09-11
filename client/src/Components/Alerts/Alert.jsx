7710778dfed8140037cbdcc100c23bd256f2feec
choijake0207 <jake.ch0712@gmail.com>
2024-09-10 18:30:42 -0700

    add switch case render for alert action type

--- client/src/Components/Alerts/Alert.jsx
+++ client/src/Components/Alerts/Alert.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react'
 import "./alert.css"
-import { X } from 'phosphor-react';
+import { X, Pause, ArrowCounterClockwise, Target, Play } from 'phosphor-react';
 
-export default function Alert({message, type, onClose}) {
+export default function Alert({message, type, action, onClose}) {
     useEffect(() => {
         const timer = setTimeout(() => {
+          document.getElementById("alert-box").classList.add("slide-out")
+          setTimeout(() => {
             onClose()
-        }, 3000);
+          }, 100)
+        }, 2000);
         return () => clearTimeout(timer)
     }, [])
   return (
-    <div className={`alert ${type}-alert`}>
-        <p>{message}</p>
-        <button onClick={onClose}><X/></button>
+    <div id="alert-box" className={`alert ${type} ${action}`}>
+        {(() => {
+          switch (action) {
+            case "reset":
+              return <div><ArrowCounterClockwise/><p>{message}</p></div>
+            case "pause":
+              return <div><Pause/><p>{message}</p></div>
+            case "resume":
+              return <div><Play/><p>{message}</p></div>
+          }
+        })()}
+        <button onClick={onClose}><X weight="bold"/></button>
     </div>
   )
 }
