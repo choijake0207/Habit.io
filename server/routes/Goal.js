819424ecb56a3114bb7f39f479d3c3af88ce5aa2
choijake0207 <jake.ch0712@gmail.com>
2024-09-11 10:11:31 -0700

    remove individual goal router

--- server/routes/Goal.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {Habit} = require("../models")
-const {validateToken} = require("../middleware/AuthMiddleware")
-
-
-
-
-module.exports = router
