f6128626cb09148d01ef9823294f10fb2a756ad0
choijake0207 <jake.ch0712@gmail.com>
2024-09-12 14:18:16 -0700

    move calculateProgress func outside of useEffect and use to initialize progress state to avoid blank initial render

--- client/src/Components/Progress/Progress.jsx
+++ client/src/Components/Progress/Progress.jsx
@@ -4,7 +4,7 @@ import "./Progress.css"
 
 
 export default function Progress({goal, type, habit}) {
-  const [progress, setProgress] = useState(0)
+ 
 
   const convertHabitToISO = (habitStart) => {
     const [time, date] = habitStart.split("-")
@@ -14,39 +14,43 @@ export default function Progress({goal, type, habit}) {
     return parsedTime
   }
 
-  useEffect(() => {
-    const calculateProgress = () => {
-      const now = new Date()
-      let percentage = 0
-      const habitStart = convertHabitToISO(habit.startDate)
-      let start = goal.countStreak ? habitStart : goal.start
-      if (goal.type === "date") {
-        const targetDate = new Date(goal.target)
-        const totalDuration = targetDate - new Date(start)
-        const passedDuration = now - new Date(start)
-        console.log(passedDuration)
-        percentage = (passedDuration / totalDuration ) * 100
-      } else if (goal.type === "duration") {
-        const {length, unit} = goal.target
-        const unitToms = {
-          Hours: 1000 * 60 * 60,
-          Days: 1000 * 60 * 60 * 24,
-          Weeks: 1000 * 60 * 60 * 24 * 7,
-          Months: 1000 * 60 * 60 * 24 * 30,
-          Years: 1000 * 60 * 60 * 24 * 365,
-        }
-        const totalDuration = length * unitToms[unit]
-        console.log(totalDuration)
-        const passedDuration = now - new Date(start)
-        console.log(passedDuration)
-        percentage = (passedDuration / totalDuration) * 100
-      }
-      setProgress(Math.min(100, percentage))
-    }
 
-    const interval = setInterval(calculateProgress, 1000)
-    return () => clearInterval(interval)
-  }, [goal])
+  const calculateProgress = () => {
+    const now = new Date()
+    let percentage = 0
+    const habitStart = convertHabitToISO(habit.startDate)
+    let start = goal.countStreak ? habitStart : goal.start
+    if (goal.type === "date") {
+      const targetDate = new Date(goal.target)
+      const totalDuration = targetDate - new Date(start)
+      const passedDuration = now - new Date(start)
+      console.log(passedDuration)
+      percentage = (passedDuration / totalDuration ) * 100
+    } else if (goal.type === "duration") {
+      const {length, unit} = goal.target
+      const unitToms = {
+        Hours: 1000 * 60 * 60,
+        Days: 1000 * 60 * 60 * 24,
+        Weeks: 1000 * 60 * 60 * 24 * 7,
+        Months: 1000 * 60 * 60 * 24 * 30,
+        Years: 1000 * 60 * 60 * 24 * 365,
+      }
+      const totalDuration = length * unitToms[unit]
+      console.log(totalDuration)
+      const passedDuration = now - new Date(start)
+      console.log(passedDuration)
+      percentage = (passedDuration / totalDuration) * 100
+    }
+    return Math.min(100, percentage); 
+  }
+
+  const [progress, setProgress] = useState(() => calculateProgress())
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(calculateProgress())
+    }, 1000)
+    return (() => clearInterval(interval))
+  }, [])
 
 
   const roundedProgress = Math.round(progress)
