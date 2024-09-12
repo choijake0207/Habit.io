332f3369c1648b8a18ef014c66be6fef98fc62af
choijake0207 <jake.ch0712@gmail.com>
2024-09-12 11:54:48 -0700

    add progress bar in card view

--- client/src/Components/Habit/HabitCard.jsx
+++ client/src/Components/Habit/HabitCard.jsx
@@ -2,8 +2,9 @@ import React,{useState} from 'react'
 import "./HabitCard.css"
 import Timer from '../Timer/Timer'
 import {useNavigate} from "react-router-dom"
+import Progress from '../Progress/Progress'
 
-export default function HabitCard({habit, forDisplay, color}) {
+export default function HabitCard({habit, forDisplay, color, inputName}) {
 
     const [time, date] = forDisplay ? new Date().toLocaleTimeString().split("-") : habit.startDate.split("-")
     const navigate = useNavigate()
@@ -14,18 +15,22 @@ export default function HabitCard({habit, forDisplay, color}) {
     <li className={`habit-card ${forDisplay ? color : habit.color}`}
       onClick={forDisplay ? null : () => navigate(`/habit/${habit.id}`)}
     >
-        <h3>{forDisplay ? "New Habit" : habit.name}</h3>
+        <h3>{forDisplay ? inputName : habit.name}</h3>
         <p>Started on {date}</p>
         {forDisplay ? 
           <h4>0 Hours 0 Minutes</h4>
         
-        : <Timer
-            start={habit.startDate}
-            type={"Hours"}
-            status={habit.status}
-            pauseDuration={habit.pauseDuration}
-            pauseDate={habit.pauseDate}
-          />
+        : 
+          <>
+            <Timer
+              start={habit.startDate}
+              type={"Days"}
+              status={habit.status}
+              pauseDuration={habit.pauseDuration}
+              pauseDate={habit.pauseDate}
+            />
+            {habit.currentGoal && <Progress goal={habit.currentGoal} type={"card"}/>}
+          </>
         }
 
     </li>
