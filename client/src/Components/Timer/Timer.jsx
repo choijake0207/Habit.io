961027c736e8c4a86c7167da76ccbd5165b25d04
choijake0207 <jake.ch0712@gmail.com>
2024-10-07 16:05:43 -0400

    fix modulo formatting in week setting

--- client/src/Components/Timer/Timer.jsx
+++ client/src/Components/Timer/Timer.jsx
@@ -46,7 +46,7 @@ export default function Timer({start, type, status, pauseDuration, pauseDate}) {
                 case "Days":
                     return <h4>{days} Days {moduloHours} Hours {minutes} Minutes</h4>
                 case "Weeks":
-                    return <h4> {weeks} Weeks {moduloDays} Days {hours} Hours </h4>
+                    return <h4> {weeks} Weeks {days} Days {moduloHours} Hours </h4>
                 case "Months":
                     return <h4>{months} Months {weeks} Weeks {days} Days  </h4>
                 case "Years":
